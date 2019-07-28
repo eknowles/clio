7c2e0ec6bee860b36cf5db642be3ead4fecc2f4e
Edward Knowles <ed.knowles@stunjelly.com>
2019-07-28 18:12:25 +0100

    feat(config): add json schema for config and validation

--- packages/config/src/index.js
+++ packages/config/src/index.js
@@ -1,6 +1,17 @@
 import rc from 'rc';
+import Ajv from 'ajv';
+
+import schema from './schema.json';
 
 const APP_NAME = 'clio';
 const config = rc(APP_NAME);
 
-module.exports = config;
+const ajv = new Ajv({ allErrors: true });
+const validate = ajv.compile(schema);
+const valid = validate(config);
+
+if (!valid) {
+  throw new Error(validate.errors[0].message);
+}
+
+export default config;
